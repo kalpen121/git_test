50a5c896cfe1f4e7dc0f97bc8851f480e414adf2
kalpen121 <kalpen@121group.io>
2021-02-28 13:46:07 +0530

    Git Testing

---
+++ app/src/main/java/com/pals/gittest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.pals.gittest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppCompatImageView img_test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img_test = findViewById(R.id.img_test);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Glide.with(MainActivity.this)
+                .load("https://api.androidhive.info/images/sample.jpg")
+                .into(img_test);
+    }
+}
