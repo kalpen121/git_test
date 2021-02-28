893922f8891886459378f40486167c44137298b4
kalpen121 <kalpen@121group.io>
2021-02-28 15:14:01 +0530

    Git Testing

--- app/src/main/java/com/pals/gittest/MainActivity.java
+++ app/src/main/java/com/pals/gittest/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img_test = findViewById(R.id.img_test);
+//        img_test = findViewById(R.id.img_test);
     }
 
     @Override
